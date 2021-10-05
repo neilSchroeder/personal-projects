55a24c3bdcb32f700d3381fad3b0bc1ca72a33e2
Neil Schroeder <schr1077@umn.edu>
2021-10-04 22:35:36 -0500

    updated graph_bfs_class with pep8 formatting

--- shortest_path_stargate/graph_bfs_class.py
+++ shortest_path_stargate/graph_bfs_class.py
@@ -1,7 +1,9 @@
 import time
-import operator
 from node_bfs_class import node
+
+
 class graph:
+    """a container structure for nodes"""
 
     def __init__(self, map, watch_evolution=True):
         self.map = map
@@ -13,70 +15,98 @@ class graph:
         self.populate_nodes()
 
     def get_start_node_coords(self):
+        """gets the coordinates of the start node"""
         i = ['S' in row for row in self.map].index(True)
-        j = [x=='S' for x in self.map[i]].index(True)
-        return (i,j)
+        j = [x == 'S' for x in self.map[i]].index(True)
+        return (i, j)
 
     def get_goal_node_coords(self):
+        """gets the coordinates of the goal node"""
         i = ['G' in row for row in self.map].index(True)
         j = [x == 'G' for x in self.map[i]].index(True)
-        return (i,j)
+        return (i, j)
 
     def populate_nodes(self):
-        self.nodes = {(i,j):node(self.map,
-                          (i,j),
-                          (i,j)==self.goal_node_coords,
-                          (i,j)==self.start_node_coords) for i in range(len(self.map)) for j in range(len(self.map[i])) if self.map[i][j] != 'X' }
+        self.nodes = {(i, j): node(self.map,
+                      (i, j),
+                      (i, j) == self.goal_node_coords,
+                      (i, j) == self.start_node_coords)
+                      for i in range(len(self.map))
+                      for j in range(len(self.map[i]))
+                      if self.map[i][j] != 'X'}
 
-    def distance_to_goal(self,n):
-        return (abs(self.goal_node_coords[0]-n[0])**2 + abs(self.goal_node_coords[1]-n[1])**2)**0.5
+    def distance_to_goal(self, n):
+        """ returns euclidean distance to goal"""
+        return (
+            abs(self.goal_node_coords[0]-n[0])**2
+            + abs(self.goal_node_coords[1]-n[1])**2
+        )**0.5
 
-    def distance_to_start(self,n):
-        return (abs(self.start_node_coords[0]-n[0])**2 + abs(self.start_node_coords[1]-n[1])**2)**0.5
+    def distance_to_start(self, n):
+        """ returns the euclidean distance to the start"""
+        return (
+            abs(self.start_node_coords[0]-n[0])**2
+            + abs(self.start_node_coords[1]-n[1])**2
+        )**0.5
 
-    def topo_sort(self,keys):
-        return sorted(sorted(keys, key=lambda x: self.active_nodes[x].indegree), key=lambda x: self.distance_to_start(x))
+    def topo_sort(self, keys):
+        """ sorts the keys sorted first by indegree, then by dist to start """
+        return sorted(
+            sorted(keys,
+                   key=lambda x: self.active_nodes[x].indegree),
+            key=lambda x: self.distance_to_start(x))
 
     def search_for_goal(self):
-
+        """ starts the directed breadth first search for the goal """
         queue = [self.start_node_coords]
-
-        count = 0
         while queue:
-            #activate the nodes in the queue
+            # activate the nodes in the queue
             queue_indices = {}
             n = self.nodes[queue.pop(0)]
             if n.is_start:
                 n.set_indegree(())
 
-
             if not n.is_active:
                 n.get_neighboring_nodes()
                 self.active_nodes[n.loc] = n
                 if not n.is_start:
-                    min_indegree = min([self.nodes[key].indegree for key in n.neighbors.keys() if self.nodes[key].is_active])
+                    # set the indegree
+                    min_indegree = min(
+                        [self.nodes[key].indegree
+                         for key in n.neighbors.keys()
+                         if self.nodes[key].is_active])
                     n.set_indegree(min_indegree)
+                # if the goal hasn't been visited continue adding to the queue
                 if not self.nodes[self.goal_node_coords].is_active:
-                    queue = queue + [key for key in n.neighbors.keys() if not self.nodes[key].is_active]
+                    queue = (queue + [key for key in n.neighbors.keys()
+                                      if not self.nodes[key].is_active])
                     queue_indices = {x: queue.index(x) for x in queue}
-                    queue.sort(key=lambda x: 0.35*queue_indices[x] + 3*n.indegree + 0.32*self.distance_to_goal(x))
+                    queue.sort(key=lambda x:
+                               0.35*queue_indices[x]
+                               + 3*n.indegree
+                               + 0.32*self.distance_to_goal(x))
                 n.activate()
 
+            # allows user to watch the search progression
             if self.watch_evolution:
                 self.draw_path(queue, reset=True, symb='Q')
                 time.sleep(0.005)
-
 
         if not self.nodes[self.goal_node_coords].is_active:
             return False
 
         return True
 
-    def get_path_length(self,path):
-        return sum([self.nodes[path[i]].distance_to_node(path[i+1]) for i in range(len(path)-1) ])
+    def get_path_length(self, path):
+        """ gets the total euclidean length of a given path """
+        return sum(
+            [self.nodes[path[i]].distance_to_node(path[i+1])
+             for i in range(len(path)-1)])
 
     def find_shortest_path(self):
-
+        """
+        check activated nodes and determine the shortest path to the start
+        """
         queue = [self.start_node_coords]
         parent_path_length = {}
 
@@ -86,17 +116,28 @@ class graph:
             self.active_nodes[key].set_distance_from_start(self.active_nodes)
 
             if not queue.count(self.goal_node_coords):
-                neighbors = [ x for x in self.nodes[key].neighbors if self.nodes[x].is_active and self.nodes[x].indegree > self.nodes[key].indegree and queue.count(x) < 2]
+                neighbors = [x for x in self.nodes[key].neighbors
+                             if self.nodes[x].is_active
+                             and
+                             self.nodes[x].indegree > self.nodes[key].indegree
+                             and queue.count(x) < 2]
                 queue = queue + neighbors
                 for neighbor in neighbors:
                     if neighbor in parent_path_length.keys():
-                        parent_length = self.nodes[key].distance_from_start_node
+                        parent_length = (
+                            self.nodes[key].distance_from_start_node
+                        )
                         if parent_length < parent_path_length[neighbor]:
                             parent_path_length[neighbor] = parent_length
                     else:
-                        parent_path_length[neighbor] = self.nodes[key].distance_from_start_node
+                        parent_path_length[neighbor] = (
+                            self.nodes[key].distance_from_start_node
+                        )
 
-            queue = sorted(sorted(queue, key = lambda x : self.active_nodes[x].indegree), key = lambda x : parent_path_length[x])
+            queue = sorted(
+                sorted(queue,
+                       key=lambda x: self.active_nodes[x].indegree),
+                key=lambda x: parent_path_length[x])
             if self.watch_evolution:
                 self.draw_path(queue, reset=True, symb='Q')
                 time.sleep(0.005)
@@ -104,20 +145,24 @@ class graph:
             if key == self.goal_node_coords:
                 continue
 
-        self.active_nodes[self.goal_node_coords].set_distance_from_start(self.active_nodes)
+        gnc = self.goal_node_coords
+        self.active_nodes[gnc].set_distance_from_start(self.active_nodes)
         self.draw_path(self.nodes[self.goal_node_coords].path)
 
-    def draw_path(self, path,reset=False, symb="P"):
-        self.map[self.start_node_coords[0]][self.start_node_coords[1]] = "\033[93mS\033[0m"
-        self.map[self.goal_node_coords[0]][self.goal_node_coords[1]] = "\033[93mG\033[0m"
+    def draw_path(self, path, reset=False, symb="P"):
+        """draws a path, and resets the board if necessary"""
+        snc = self.start_node_coords
+        gnc = self.goal_node_coords
+        self.map[snc[0]][snc[1]] = "\033[93mS\033[0m"
+        self.map[gnc[0]][gnc[1]] = "\033[93mG\033[0m"
 
         for coord in path:
-            if coord != self.start_node_coords and coord != self.goal_node_coords:
+            if coord != snc and coord != gnc:
                 self.map[coord[0]][coord[1]] = "\033[93m"+symb+"\033[0m"
 
         if reset:
             print('\n'.join([''.join(row) for row in self.map]))
             print()
             for coord in path:
-                if coord != self.start_node_coords and coord != self.goal_node_coords:
+                if coord != snc and coord != gnc:
                     self.map[coord[0]][coord[1]] = '.'
