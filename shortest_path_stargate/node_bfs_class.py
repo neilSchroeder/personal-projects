95cca62def06bd656a88744674c2f95cc5f1d9f9
Neil Schroeder <schr1077@umn.edu>
2021-10-04 22:07:42 -0500

    updated node_path_bfs with pep8 formatting

--- shortest_path_stargate/node_bfs_class.py
+++ shortest_path_stargate/node_bfs_class.py
@@ -1,5 +1,5 @@
 class node:
-
+    """class for nodes in the map"""
     def __init__(self, map, location, is_goal, is_start):
         self.map = map
         self.loc = location
@@ -15,6 +15,7 @@ class node:
         self.is_active = True
 
     def set_distance_from_start(self, active_nodes):
+        """ sets the minimum distance to the start node """
         if self.is_start:
             self.path = [self.loc]
             self.distance_from_start_node = 0
@@ -33,35 +34,44 @@ class node:
                 self.distance_from_start_node = self.distance_to_node(key)
                 self.path.append(key)
                 return
-            if self.distance_to_node(key) + active_nodes[key].distance_from_start_node <= self.distance_from_start_node:
-                self.distance_from_start_node = self.distance_to_node(key) + active_nodes[key].distance_from_start_node
+            dist = (self.distance_to_node(key)
+                    + active_nodes[key].distance_from_start_node
+                    )
+            if dist <= self.distance_from_start_node:
+                self.distance_from_start_node = dist
                 min_key = key
                 available_keys.append(min_key)
 
         self.path = active_nodes[min_key].path + [min_key]
 
     def get_neighboring_nodes(self):
+        """ gets the coordinates of nodes around this node """
         max_row = len(self.map)
         max_col = len(self.map[0])
         i_min = self.loc[0]-1 if self.loc[0]-1 > 0 else 0
         i_max = self.loc[0]+2 if self.loc[0]+2 < max_row else max_row
         j_min = self.loc[1]-1 if self.loc[1]-1 > 0 else 0
         j_max = self.loc[1]+2 if self.loc[1]+2 < max_col else max_col
-        possible_neighbors = [(i,j) for i in range(i_min, i_max) for j in range(j_min, j_max)]
+        possible_neighbors = [(i, j)
+                              for i in range(i_min, i_max)
+                              for j in range(j_min, j_max)]
         for n in possible_neighbors:
             if self.map[n[0]][n[1]] != 'X' and n is not self.loc:
                 self.neighbors[n] = self.distance_to_node(n)
 
     def distance_to_node(self, n):
+        """returns the distance to a node with coodinates n"""
         return (abs(self.loc[0]-n[0])**2 + abs(self.loc[1]-n[1])**2)**0.5
 
     def print(self):
-        print('location:',self.loc)
-        print('neighbors:',self.neighbors)
+        """prints out information about this node """
+        print('location:', self.loc)
+        print('neighbors:', self.neighbors)
         print('distance to start node:', self.distance_from_start_node)
         print('path to start node:', self.path)
 
     def set_indegree(self, parent_indegree):
+        """sets the indegree of the node based on it's parent node"""
         if self.is_start:
             self.indegree = 0
         else:
