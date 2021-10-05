c9a07739188f4eb2d3b9fdaec3f86a834274ab2e
Neil Schroeder <schr1077@umn.edu>
2021-10-04 22:00:13 -0500

    updated shortest_path_bfs with pep8 formatting

--- shortest_path_stargate/shortest_path_bfs.py
+++ shortest_path_stargate/shortest_path_bfs.py
@@ -1,13 +1,15 @@
-import time
+
 from graph_bfs_class import graph
 
+
 def shortest_path_bfs(map_string, watch_evolution=True):
+    """ control function for finding the shortest path """
 
     map_list = map_string.split('\n')
 
     map_list = [list(row) for row in map_list]
 
-    my_graph = graph(map_list,watch_evolution)
+    my_graph = graph(map_list, watch_evolution)
     if my_graph.search_for_goal():
         my_graph.find_shortest_path()
         return '\n'.join([''.join(row) for row in my_graph.map])
