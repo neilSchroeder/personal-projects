1edb824bf29dc7444d56bd221d5065f8e1e64507
Neil Schroeder <schr1077@umn.edu>
2021-10-15 23:12:18 -0500

    moved control script to test for the sake of being compact

--- shortest_path_stargate/test_shortest_path.py
+++ shortest_path_stargate/test_shortest_path.py
@@ -1,7 +1,18 @@
-
-from shortest_path_bfs import shortest_path_bfs
 import time
 import random
+
+def shortest_path_bfs(map_string, watch_evolution=True):
+    """ control function for finding the shortest path """
+    map_list = map_string.split('\n')
+
+    map_list = [list(row) for row in map_list]
+
+    my_graph = graph(map_list, watch_evolution)
+    if my_graph.a_star_search():
+        #my_graph.create_gif()
+        return '\n'.join([''.join(row) for row in my_graph.map])
+
+    return "Oh for crying out loud..."
 
 
 def main():
